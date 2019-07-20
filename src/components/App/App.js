c36f16aa9071a32e0b954b5647b966789958f34a
Molly Ellison <vonnegutter1@gmail.com>
2019-07-20 10:33:21 -0500

    confirmed comments route working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route} from 'react-router-dom';
 
+//Routes Listed Below
+
+import Comments from '../Routes/Comments/Comments';
+
+
+// App Component with routes included below
 class App extends Component {
+
+  startSurvey = () => {
+    console.log("started survey")
+    
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
+          
         </header>
         <br/>
+        <button onClick={(event) => this.startSurvery()}>Start Survey</button>
+        <br/>
+        <div>
+        <Router>
+          <Route path="/Comments" component={Comments} />
+        </Router>
+       
+        </div>  
       </div>
     );
   }
---
+++ src/components/Routes/Comments/Comments.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import { HashRouter as Router, Route } from 'react-router-dom';
+
+class Comments extends Component {
+
+  
+
+    render() {
+        return (
+           <>
+                    <h4>Comments</h4>
+
+                
+                </>
+        ) 
+    }
+}
+
+export default Comments;
